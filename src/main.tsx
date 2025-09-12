2f30fa2c14206d2c57e6264c60cfb1656d29bd21
mayada-shaaban <Mayada@i-legal.me>
2025-09-12 19:32:24 +0300

    update routings to match new file structure

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter } from 'react-router';
 import { RouterProvider } from 'react-router/dom';
-import App from './App';
-import Favorites from './Favorites';
+import Home from './pages/Home';
+import Favorites from './pages/Favorites';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    Component: App,
+    Component: Home,
   },
   {
     path: '/favorites',
