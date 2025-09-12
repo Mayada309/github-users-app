63c5c984af1d64c451503725a4cc833411d7a818
mayada-shaaban <Mayada@i-legal.me>
2025-09-12 18:45:54 +0300

    install needed packages and setup routings

---
+++ src/Favorites.tsx
@@ -0,0 +1,11 @@
+import { NavLink } from 'react-router';
+
+function Favorites() {
+  return (
+    <div>
+      <NavLink to={'/'}>Home</NavLink>
+    </div>
+  );
+}
+
+export default Favorites;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router/dom';
+import App from './App';
+import Favorites from './Favorites';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: App,
+  },
+  {
+    path: '/favorites',
+    Component: Favorites,
+  },
+]);
+
+const root = document.getElementById('root')!;
+
+createRoot(root).render(<RouterProvider router={router} />);
