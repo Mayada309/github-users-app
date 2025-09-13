f65528a0741c72bd608d90ddfd741294755ac6db
mayada-shaaban <Mayada@i-legal.me>
2025-09-13 17:40:56 +0300

    fix users' search logic

--- src/utils/handle-search-params.ts
+++ src/utils/handle-search-params.ts
@@ -1,14 +1,15 @@
 export const handleSearchParam = (param: string, value: string) => {
   const searchParams = new URLSearchParams(window.location.search);
-  if (searchParams.get(param)) {
-    return;
-  }
-  if (value.length === 0 || value === '' || !value || value === null) {
-    const searchParams = new URLSearchParams(window.location.search);
+  const valueIsEmpty = !value || value.trim() === '';
+
+  if (valueIsEmpty) {
     searchParams.delete(param);
-    window.history.pushState({}, '', `${window.location.pathname}`);
+    const newSearch = searchParams.toString();
+    const newUrl = newSearch
+      ? `${window.location.pathname}?${newSearch}`
+      : window.location.pathname;
+    window.history.pushState({}, '', newUrl);
   } else {
-    const searchParams = new URLSearchParams(window.location.search);
     searchParams.set(param, value);
     window.history.pushState(
       {},
